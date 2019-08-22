f3e7cfcfe40cf1bce1973ccf37cb4676bc5272d6
ZPPD <zhana.p.d@gmail.com>
2019-08-22 12:41:01 -0400

    created recipe list

--- src/App.js
+++ src/App.js
@@ -7,12 +7,35 @@ import RecipeDetails from "./components/RecipeDetails";
 
 class App extends React.Component {
   state = {
-    recipes: []
+    // recipes: [],
+    recipes: recipes,
+    url:
+      "https://www.food2fork.com/api/search?key=71e7bc1e0a580b76ebde11b3684907cc"
   };
+
+  // method to get the recipes
+  // async getRecipes() {
+  //   try {
+  //     const data = await fetch(this.state.url);
+  //     const jsonData = await data.json();
+
+  //     this.setState({
+  //       recipes: jsonData.recipes
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+
+  // componentDidMount() {
+  //   this.getRecipes();
+  // }
+
   render() {
+    // console.log(this.state.recipes);
     return (
       <>
-        <RecipeList />
+        <RecipeList recipes={this.state.recipes} />
         <RecipeDetails />
       </>
     );
