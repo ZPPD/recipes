b78b5f2432b9ce0a3e1537bb79149b5ca68af893
ZPPD <zhana.p.d@gmail.com>
2019-08-22 11:53:46 -0400

    created initial components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { recipes } from "./tempList";
+import RecipeList from "./components/RecipeList";
+import RecipeDetails from "./components/RecipeDetails";
+
+class App extends React.Component {
+  state = {
+    recipes: []
+  };
+  render() {
+    return (
+      <>
+        <RecipeList />
+        <RecipeDetails />
+      </>
+    );
+  }
 }
 
 export default App;
