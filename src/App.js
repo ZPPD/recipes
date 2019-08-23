bd7a54bc85c7361e26be4ac85b3b4adc08d1c840
ZPPD <zhana.p.d@gmail.com>
2019-08-23 12:00:54 -0400

    recipe details

--- src/App.js
+++ src/App.js
@@ -10,33 +10,46 @@ class App extends React.Component {
     // recipes: [],
     recipes: recipes,
     url:
-      "https://www.food2fork.com/api/search?key=71e7bc1e0a580b76ebde11b3684907cc"
+      "https://www.food2fork.com/api/search?key=71e7bc1e0a580b76ebde11b3684907cc",
+    details_id: 35375,
+    pageIndex: 1
   };
 
   // method to get the recipes
-  // async getRecipes() {
-  //   try {
-  //     const data = await fetch(this.state.url);
-  //     const jsonData = await data.json();
+  async getRecipes() {
+    try {
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
 
-  //     this.setState({
-  //       recipes: jsonData.recipes
-  //     });
-  //   } catch (error) {
-  //     console.log(error);
-  //   }
-  // }
+      this.setState({
+        recipes: jsonData.recipes
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
-  // componentDidMount() {
-  //   this.getRecipes();
-  // }
+  componentDidMount() {
+    this.getRecipes();
+  }
+
+  displayPage = index => {
+    switch (index) {
+      default:
+      case 1:
+        return <RecipeList recipes={this.state.recipes} />;
+      case 0:
+        return <RecipeDetails id={this.state.details_id} />;
+    }
+  };
 
   render() {
     // console.log(this.state.recipes);
     return (
       <>
-        <RecipeList recipes={this.state.recipes} />
-        <RecipeDetails />
+        {/* <RecipeList recipes={this.state.recipes} /> */}
+        {/* <RecipeDetails id={this.state.details_id} /> */}
+        {this.displayPage(this.state.pageIndex)}
       </>
     );
   }
